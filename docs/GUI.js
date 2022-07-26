88f82754f9df994c6c68f515af2eb8cb06de15c8
paulo <paulo@DESKTOP-6JRUH77>
2022-07-26 14:10:56 -0300

    Block moves when game is over

--- docs/GUI.js
+++ docs/GUI.js
@@ -2,15 +2,17 @@ import CellState from './CellState.js';
 import Joust from './Joust.js';
 import Cell from './Cell.js';
 
-function GUI() {
-    let game = new Joust(8, 8);
-    const images = { PLAYER1: "White-Knight.svg", PLAYER2: "Black-Knight.svg" };
-    function init() {
+class GUI {
+    constructor() {
+        this.game = null;
+        this.images = { PLAYER1: "White-Knight.svg", PLAYER2: "Black-Knight.svg" };    
+    }
+    init() {
         let form = document.forms[0];
         let cols = parseInt(form.cols.value);
         let rows = parseInt(form.rows.value);
-        game = new Joust(rows, cols);
-        let tab = game.getBoard();
+        this.game = new Joust(rows, cols);
+        let tab = this.game.getBoard();
         let tbody = document.querySelector("tbody");
         tbody.innerHTML = "";
         for (let i = 0; i < tab.length; i++) {
@@ -19,23 +21,28 @@ function GUI() {
                 let td = document.createElement("td");
                 if (tab[i][j] !== CellState.EMPTY) {
                     let img = document.createElement("img");
-                    img.src = `images/${images[tab[i][j]]}`;
+                    img.src = `images/${this.images[tab[i][j]]}`;
                     td.appendChild(img);
                 }
-                td.onclick = play;
+                td.onclick = this.play.bind(this);
                 tr.appendChild(td);
             }
             tbody.appendChild(tr);
         }
-        changeMessage();
+        this.changeMessage();
     }
-    function play() {
-        let image = document.querySelector(`img[src*="${images[game.getTurn()]}"]`);
+    unregisterEvents() {
+        let cells = document.querySelectorAll("td");
+        cells.forEach(c => c.onclick = undefined);
+    }
+    play(evt) {
+        let image = document.querySelector(`img[src*="${this.images[this.game.getTurn()]}"]`);
         let origin = image.parentNode;
         try {
-            let begin = coordinates(origin);
-            let end = coordinates(this);
-            let mr = game.move(begin, end);
+            const destination = evt.target;
+            let begin = this.coordinates(origin);
+            let end = this.coordinates(destination);
+            let mr = this.game.move(begin, end);
             let { x: or, y: oc } = begin;
             let { x: dr, y: dc } = end;
             const time = 1000;
@@ -43,42 +50,43 @@ function GUI() {
             anim.onfinish = () => {
                 origin.textContent = "X";
                 origin.className = "blocked";
-                this.innerHTML = "";
-                this.appendChild(image);
-                changeMessage(mr);
+                destination.innerHTML = "";
+                destination.appendChild(image);
+                this.changeMessage(mr);
             };
         } catch (ex) {
-            setMessage(ex.message);
+            this.setMessage(ex.message);
         }
     }
-    function coordinates(cell) {
+    coordinates(cell) {
         return new Cell(cell.parentNode.rowIndex, cell.cellIndex);
     }
-    function setMessage(msg) {
+    setMessage(msg) {
         let message = document.getElementById("message");
         message.innerHTML = msg;
     }
-    function changeMessage(m) {
-        hidePossibleMoves();
+    changeMessage(m) {
+        this.hidePossibleMoves();
         let objs = { DRAW: "Draw!", PLAYER2: "Black's win!", PLAYER1: "White's win!" };
         if (objs[m]) {
-            setMessage(`Game Over! ${objs[m]}`);
+            this.setMessage(`Game Over! ${objs[m]}`);
+            this.unregisterEvents();
         } else {
             let msgs = { PLAYER1: "White's turn.", PLAYER2: "Black's turn." };
-            setMessage(msgs[game.getTurn()]);
-            showPossibleMoves();
+            this.setMessage(msgs[this.game.getTurn()]);
+            this.showPossibleMoves();
         }
     }
-    function showPossibleMoves() {
-        let image = document.querySelector(`img[src*="${images[game.getTurn()]}"]`);
+    showPossibleMoves() {
+        let image = document.querySelector(`img[src*="${this.images[this.game.getTurn()]}"]`);
         let origin = image.parentNode;
-        let moves = game.possibleMoves(coordinates(origin));
+        let moves = this.game.possibleMoves(this.coordinates(origin));
         for (let { x, y } of moves) {
             let tempCell = document.querySelector(`tr:nth-child(${x + 1}) td:nth-child(${y + 1})`);
             tempCell.className = 'selected';
         }
     }
-    function hidePossibleMoves() {
+    hidePossibleMoves() {
         let cells = document.querySelectorAll("td");
         cells.forEach(c => {
             if (c.className !== 'blocked') {
@@ -86,13 +94,12 @@ function GUI() {
             }
         });
     }
-    function registerEvents() {
+    registerEvents() {
         let form = document.forms[0];
-        form.rows.onchange = init;
-        form.cols.onchange = init;
-        init();
+        form.rows.onchange = this.init.bind(this);
+        form.cols.onchange = this.init.bind(this);
+        this.init();
     }
-    return { registerEvents };
 }
-let gui = GUI();
+let gui = new GUI();
 gui.registerEvents();
--- docs/Joust.js
+++ docs/Joust.js
@@ -3,75 +3,81 @@ import Player from './Player.js';
 import Cell from './Cell.js';
 import Winner from './Winner.js';
 
-export default function Joust(nrows, ncols) {
-    const rows = nrows;
-    const cols = ncols;
-    let board = startBoard();
-    let turn = Player.PLAYER1;
-    function getBoard() {
-        return board;
+export default class Joust {
+    constructor(nrows, ncols) {
+        this.rows = nrows;
+        this.cols = ncols;
+        this.turn = Player.PLAYER1;    
+        this.board = this.startBoard();
+        this.winner = Winner.NONE;
     }
-    function startBoard() {
-        let matrix = Array(rows).fill().map(() => Array(cols).fill(CellState.EMPTY));
+    getBoard() {
+        return this.board;
+    }
+    startBoard() {
+        let matrix = Array(this.rows).fill().map(() => Array(this.cols).fill(CellState.EMPTY));
         let players = [CellState.PLAYER1, CellState.PLAYER2];
         for (let i = 0; i < players.length;) {
-            let row = Math.floor(Math.random() * rows);
-            let col = Math.floor(Math.random() * cols);
+            let row = Math.floor(Math.random() * this.rows);
+            let col = Math.floor(Math.random() * this.cols);
             if (matrix[row][col] === CellState.EMPTY) {
                 matrix[row][col] = players[i++];
             }
         }
         return matrix;
     }
-    function getTurn() {
-        return turn;
+    getTurn() {
+        return this.turn;
     }
-    function move(beginCell, endCell) {
+    move(beginCell, endCell) {
+        if(this.winner !== Winner.NONE) {
+            return this.winner;
+        }
         let { x: or, y: oc } = beginCell;
         let { x: dr, y: dc } = endCell;
         if (!beginCell || !endCell) {
             throw new Error("The value of one of the cells does not exist.");
         }
-        let currentPiece = getPiece(beginCell);
-        if ((currentPiece === CellState.PLAYER1 && turn === Player.PLAYER2) || (currentPiece === CellState.PLAYER2 && turn === Player.PLAYER1)) {
+        let currentPiece = this.getPiece(beginCell);
+        if ((currentPiece === CellState.PLAYER1 && this.turn === Player.PLAYER2) || (currentPiece === CellState.PLAYER2 && this.turn === Player.PLAYER1)) {
             throw new Error("It's not your turn.");
         }
         if (beginCell.equals(endCell)) {
             throw new Error("Origin and destination must be different.");
         }
-        if (!onBoard(beginCell) || !onBoard(endCell)) {
+        if (!this.onBoard(beginCell) || !this.onBoard(endCell)) {
             throw new Error("Origin or destination are not in the board.");
         }
         if (currentPiece === CellState.EMPTY) {
             throw new Error("Origin does not have a piece.");
         }
-        if (getPiece(endCell) !== CellState.EMPTY) {
+        if (this.getPiece(endCell) !== CellState.EMPTY) {
             throw new Error("Destination must be empty.");
         }
-        let moves = possibleMoves(beginCell);
+        let moves = this.possibleMoves(beginCell);
         if (!moves.some(z => z.equals(endCell))) {
             throw new Error("This move is invalid.");
         }
-        /* Realizar movimento */
-        board[dr][dc] = board[or][oc];
-        board[or][oc] = CellState.BLOCKED;
-        turn = (turn === Player.PLAYER1) ? Player.PLAYER2 : Player.PLAYER1;
-        return endOfGame();
+        this.board[dr][dc] = this.board[or][oc];
+        this.board[or][oc] = CellState.BLOCKED;
+        this.turn = (this.turn === Player.PLAYER1) ? Player.PLAYER2 : Player.PLAYER1;
+        this.winner = this.endOfGame();
+        return this.winner;
     }
-    function getPiece({ x, y }) {
-        return board[x][y];
+    getPiece({ x, y }) {
+        return this.board[x][y];
     }
-    function onBoard({ x, y }) {
+    onBoard({ x, y }) {
         let inLimit = (value, limit) => value >= 0 && value < limit;
-        return (inLimit(x, rows) && inLimit(y, cols));
+        return (inLimit(x, this.rows) && inLimit(y, this.cols));
     }
-    function possibleMoves({ x, y }) {
+    possibleMoves({ x, y }) {
         let positions = [new Cell(x - 2, y - 1), new Cell(x - 2, y + 1), new Cell(x + 2, y - 1), new Cell(x + 2, y + 1), new Cell(x - 1, y - 2), new Cell(x - 1, y + 2), new Cell(x + 1, y - 2), new Cell(x + 1, y + 2)];
-        return positions.filter(cell => onBoard(cell) && board[cell.x][cell.y] === CellState.EMPTY);
+        return positions.filter(cell => this.onBoard(cell) && this.board[cell.x][cell.y] === CellState.EMPTY);
     }
-    function endOfGame() {
-        let p1 = canMove(Player.PLAYER1);
-        let p2 = canMove(Player.PLAYER2);
+    endOfGame() {
+        let p1 = this.canMove(Player.PLAYER1);
+        let p2 = this.canMove(Player.PLAYER2);
         if (!p1 && !p2) {
             return Winner.DRAW;
         } else if (!p1) {
@@ -81,14 +87,13 @@ export default function Joust(nrows, ncols) {
         }
         return Winner.NONE;
     }
-    function canMove(player) {
-        let i = board.flat().indexOf(player === Player.PLAYER1 ? CellState.PLAYER1 : CellState.PLAYER2);
-        let cell = new Cell(Math.floor(i / cols), i % cols);
-        return board.flat().some((e, i) => isValidMove(cell, new Cell(Math.floor(i / cols), i % cols)));
+    canMove(player) {
+        let i = this.board.flat().indexOf(player === Player.PLAYER1 ? CellState.PLAYER1 : CellState.PLAYER2);
+        let cell = new Cell(Math.floor(i / this.cols), i % this.cols);
+        return this.board.flat().some((e, i) => this.isValidMove(cell, new Cell(Math.floor(i / this.cols), i % this.cols)));
     }
-    function isValidMove(player, endCell) {
-        let moves = possibleMoves(player);
+    isValidMove(player, endCell) {
+        let moves = this.possibleMoves(player);
         return moves.some(e => e.equals(endCell));
     }
-    return { getBoard, getTurn, move, possibleMoves };
 }
